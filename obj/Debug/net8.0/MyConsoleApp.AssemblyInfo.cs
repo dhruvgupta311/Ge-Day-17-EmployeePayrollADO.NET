78116330a4aef98d87f4ca7060afc156f925fc10
Dhruv <guptadhruv1908@gmail.com>
2024-12-30 19:28:03 +0530

    removed

--- obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+054d96d8f7fc02e60f148efa4906251e1299bdd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
