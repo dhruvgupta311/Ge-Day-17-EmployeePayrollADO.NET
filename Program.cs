5e54d11c7b9a391ee9a42844522bbd72cb22de47
Dhruv <guptadhruv1908@gmail.com>
2024-12-30 19:32:35 +0530

    uc1 commit connection

--- Program.cs
+++ Program.cs
@@ -28,31 +28,6 @@ namespace MySQLConnectionExample
                     // Open the connection to the database
                     connection.Open();
                     Console.WriteLine("Connection Successful!");
-
-                    // Example SQL query to fetch data from the employee_payroll table
-                    string query = "SELECT id, name, gender, salary, start_date FROM employee_payroll"; // Specify columns explicitly
-
-                    // Create a command object to execute the query
-                    using (MySqlCommand command = new MySqlCommand(query, connection))
-                    {
-                        // Execute the query and use a reader to access the results
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            // Check if any data is returned
-                            if (reader.HasRows)
-                            {
-                                while (reader.Read())
-                                {
-                                    // Read each row and display the data
-                                    Console.WriteLine($"ID: {reader["id"]}, Name: {reader["name"]}, Gender: {reader["gender"]}, Salary: {reader["salary"]}, Start Date: {reader["start_date"]}");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("No data found in the employee_payroll table.");
-                            }
-                        }
-                    }
                 }
             }
             catch (MySqlException mySqlEx)
--- obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78116330a4aef98d87f4ca7060afc156f925fc10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6a931c9ac94ab1de33c5ab7a452d2d879b49c7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
